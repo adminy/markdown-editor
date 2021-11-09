6353ef1b7bb2ab4d1ce00cf7a2e14b5185eb1da6
adminy <marin.bivol@dcu.ie>
2021-11-09 15:50:22 +0000

    (remove-libs) Fully NPM no libs, standard errors removed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import 'highlight.js/styles/github.css'
 import 'codemirror-spell-checker/dist/spell-checker.min.css'
 import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/base16-light.css' // needed?
-import './lib/material-icons.css'
-import './lib/github-markdown.css'
+import 'codemirror/theme/base16-light.css'
+import 'material-icons/iconfont/material-icons.css'
+import 'github-markdown-css'
 import './index.css'
-import 'codemirror/addon/mode/overlay'
 import 'codemirror/mode/gfm/gfm'
+import 'emojify.js/dist/css/basic/emojify.min.css'
+import 'emojify.js/dist/css/sprites/emojify.min.css'
+import 'emojify.js/dist/css/sprites/emojify-emoticons.min.css'
+
+// import 'codemirror/addon/mode/overlay'
 // import 'codemirror/addon/fold/markdown-fold'
 window.React = require('jsx-dom')
 const choo = require('choo')
